5fe845e5a343a76d7d5b2b9e800c2d21cfe4f37b
Duwaysan <kduwaysan@gmail.com>
2025-09-11 15:14:23 +0300

    push giphy-api-lab

--- work/week-02/day-05/02. JS-API-AJAX-Axios/exercise/script.js
+++ work/week-02/day-05/02. JS-API-AJAX-Axios/exercise/script.js
@@ -1,2 +1,71 @@
 const randomDogUrl = 'https://dog.ceo/api/breeds/image/random';
 const listAllBreedsUrl = 'https://dog.ceo/api/breeds/list/all';
+
+
+
+
+//************* CHACHED ATTRIBUTES ***************/
+
+const dgImgEl =document.getElementById("dogImage")
+const dogBreedSelectEl = document.getElementById("dogsSelectTag")
+const randomDogBtnEl = document.getElementById('getRadomDogButton')
+
+
+//************* EVENT LISTENE************* */
+dogBreedSelectEl.addEventListener("click", async (evnt) =>{
+    try{
+        //  console.log("Event listener is working",evnt.target.value, Boolean(evnt.target.value))
+         if (!Boolean(evnt.target.value))
+            return
+        // console.log('The value is truthy',evnt.target.value)
+        // dogBreed = evnt.target.value;
+        // const dogBreedURL = `https://dog.ceo/api/breeds/${dogBreed}/images`;
+        
+        const res = await fetch(`https://dog.ceo/api/breed/${evnt.target.value}/images`)
+         const data = await res.json();
+        //  console.log("Feteched data: ",data.message[0])
+        dgImgEl.setAttribute('src',data.message[0])
+        dgImgEl.setAttribute('alt',evnt.target.value)
+    }catch (err){
+        console.log(err)
+    }
+
+}
+)
+
+randomDogBtnEl.addEventListener('click',async()=>{
+    try{
+        console.log
+        const res = await fetch(randomDogUrl)
+        const data = await res.json();
+        dgImgEl.setAttribute('src',data.message[0])
+        dgImgEl.setAttribute('alt',evnt.target.value)
+    }catch(err){
+        console.log(err)
+    }
+
+
+})
+
+async function getAllDogs() {
+    try{
+        const response = await fetch(listAllBreedsUrl)
+        // console.log(response)
+        const data = await response.json();
+        // console.log(data)
+        const allDogBreeds = Object.keys(data.message)
+        allDogBreeds.forEach(breed => {
+            
+            const newOptionEl = document.createElement("OPTION")
+            newOptionEl.setAttribute("vale",breed)
+            newOptionEl.textContent = breed
+            dogBreedSelectEl.appendChild(newOptionEl)
+            // console.log(newOptionEl)
+        })
+
+    } catch (err) {
+
+    }
+}
+
+getAllDogs();
--- work/week-02/day-05/03. JS-Giphy-API-Lab (D)/js/script.js
+++ work/week-02/day-05/03. JS-Giphy-API-Lab (D)/js/script.js
@@ -1,8 +1,56 @@
 const giphyRandomEndpoint = `https://api.giphy.com/v1/gifs/random?api_key=2041494ca782403cb6055682a7943c75&tag=&rating=G`;
+const giphyTrendingEndpoint = `https://api.giphy.com/v1/gifs/trending?api_key=2041494ca782403cb6055682a7943c75&tag=&rating=G`
 
 // GRAB THE BUTTON ELEMENT FROM THE DOM
+const randomGiphyBtn = document.getElementById('getRandomGiphyButton')
+const trendGiphyBtn = document.getElementById('getTrendingGiphyButton')
+const giphyImg = document.getElementById('giphyImageTag')
+const giphyTitle = document.getElementById('giphyTitleDiv')
+const giphySearch = document.getElementById('getSearchGiphyButton')
+// console.log(randomGiphyBtn)
 
 // ADD A CLICK EVENT LISTENER
+randomGiphyBtn.addEventListener('click', async() => {
+    try{
+        const res = await fetch(giphyRandomEndpoint)
+        const data = await res.json()
+        giphyImg.setAttribute('src',data.data.images.original.url)
+        giphyTitle.innerHTML = data.data.title
+
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+
+trendGiphyBtn.addEventListener('click', async() => {
+    try{
+        const res = await fetch(giphyTrendingEndpoint)
+        const data = await res.json()
+        console.log(data)
+        
+        giphyImg.setAttribute('src',data.data[0].images.original.url)//data.data.url)
+        giphyTitle.innerHTML = data.data[0].title
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+giphySearch.addEventListener('click', async() => {
+    try{
+        const searchQuery = document.querySelector('input[name="user-input"]')
+        console.log(searchQuery.value)
+        const res = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=2041494ca782403cb6055682a7943c75&tag=${searchQuery.value}&rating=G`)
+        const data = await res.json()
+        console.log(data)
+        
+        giphyImg.setAttribute('src',data.data.images.original.url)//data.data.url)
+        giphyTitle.innerHTML = data.data.title
+    } catch (err) {
+        console.log(err)
+    }
+})
+
 
 // INSIDE THE EVENT HANDLER, MAKE A FETCH GET REQUEST TO THE
 // giphyRandomApiUrl ENDPOINT
