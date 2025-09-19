e1d6acd621e2c98eeaf19cd95805b133244c7628
Duwaysan <kduwaysan@gmail.com>
2025-09-19 13:14:37 +0300

    Lab 1 Testing

--- work/week-03/day-05/03. Python-Testing-Lab-1/src/lab.py
+++ work/week-03/day-05/03. Python-Testing-Lab-1/src/lab.py
@@ -1,23 +1,49 @@
 # 1. Vowel Counter
 def count_vowels(s):
-    pass
+    count = 0
+    for char in s:
+        if char in ['a', 'e', 'i', 'o','u']:
+            count += 1
+    return f"Number of vowels: {count}"
 
 # 2. String Occurrences of 'bob'
 def count_bob_occurrences(s):
-    pass
+    count = 0
+    for i in range(len(s)-2): #-2 to avoid index error
+        if 'bob' in s[i:i+3]:
+            count += 1
+    return f"Number of times bob occurs is: {count}."
 
 # 3. String Reversal
 def reverse_string(s):
-    pass
+    return "Output: "+s[::-1]
+    
 
 # 4. Letter Case Counter
 def count_case(s):
-    pass
+    lower = 0
+    upper = 0
+    for char in s:
+        if char == " " or char == char.lower():
+            lower += 1
+        else:
+            if char == char.upper():
+                upper += 1
+    return f"Output: UPPERCASE: {upper}, LOWERCASE: {lower}"
 
 # 5. Alphabetical Sorting of Words
 def sort_words(s):
-    pass
+    list_of_words = s.split(", ")
+    list_of_words.sort()
+    return ", ".join(list_of_words)
 
 # 6. Palindrome Checker
 def is_palindrome(s):
-    pass
+    import math #is it same from both sides ?
+    return s == s[::-1] 
+
+count_bob_occurrences('azcbobobegghakl')
+count_vowels('azcbobobegghakl')
+reverse_string('Programming in Python')
+count_case("Hello World")
+sort_words('without, hello, bag, world')
--- work/week-03/day-05/03. Python-Testing-Lab-1/tests/LabTest.py
+++ work/week-03/day-05/03. Python-Testing-Lab-1/tests/LabTest.py
@@ -1,1 +1,32 @@
 # write your tests here
+import unittest
+from src import lab
+
+class LabTest(unittest.TestCase):
+
+    def test_count_vowels(self):
+        output = lab.count_vowels('azcbobobegghakl')
+        self.assertEqual(output, 'Number of vowels: 5')
+
+    def test_count_bob_occurrences(self):
+        output = lab.count_bob_occurrences('azcbobobegghakl')
+        self.assertEqual(output, 'Number of times bob occurs is: 2.')
+
+    def test_reverse_string(self):
+        output = lab.reverse_string('Programming in Python')
+        self.assertEqual(output,'Output: nohtyP ni gnimmargorP')
+
+    def test_count_case(self):
+        output = lab.count_case("Hello World")
+        self.assertEqual(output, 'Output: UPPERCASE: 2, LOWERCASE: 9')
+    
+    def test_sort_words(self):
+        output = lab.sort_words('without, hello, bag, world')
+        self.assertEqual(output,'bag, hello, without, world' )
+
+    def test_is_palindrom(self):
+        output = lab.is_palindrome('abba')
+        self.assertIn(output, [True, False])
+
+if __name__ == '__main__':
+    unittest.main()
