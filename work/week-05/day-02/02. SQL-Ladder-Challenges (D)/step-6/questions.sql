8a71dc3a68b1f32ed169f81d07e982f93e4cbe0c
Duwaysan <kduwaysan@gmail.com>
2025-10-02 16:12:18 +0300

    step-6 Completion

--- work/week-05/day-02/02. SQL-Ladder-Challenges (D)/step-6/questions.sql
+++ work/week-05/day-02/02. SQL-Ladder-Challenges (D)/step-6/questions.sql
@@ -6,4 +6,14 @@
 -- 6.1 List all the scientists' names, their projects' names,
 -- and the hours worked by that scientist on each project,
 -- in alphabetical order of project name, then scientist name.
+SELECT scientists.name AS scientist_name,
+       projects.name AS project_name,
+       projects.hours 
+FROM scientists
+JOIN assignedto ON scientists.ssn = assignedto.scientist
+JOIN projects ON assignedto.project = projects.code
+ORDER BY project_name, scientist_name;
 -- 6.2 Select the project names which are not assigned yet
+SELECT p.name AS project_name FROM projects p
+LEFT JOIN assignedto a ON p.code = a.project
+WHERE a.project IS NULL;
