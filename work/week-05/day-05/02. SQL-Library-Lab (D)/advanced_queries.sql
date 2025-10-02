f458d710a5afabe18cec3b0dafd1a36ae2adcdba
Duwaysan <kduwaysan@gmail.com>
2025-10-02 14:24:53 +0300

    SQL Library Lab - Solved

--- work/week-05/day-05/02. SQL-Library-Lab (D)/advanced_queries.sql
+++ work/week-05/day-05/02. SQL-Library-Lab (D)/advanced_queries.sql
@@ -4,21 +4,43 @@
 \x on     
 
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
+SELECT * FROM books
+JOIN authors ON books.author_id = authors.id
+WHERE authors.name = 'George R.R. Martin';
 
 -- Find all fields (book and author related) for all books written by Milan Kundera.
-
+SELECT * FROM books b
+JOIN authors a ON b.author_id = a.id
+WHERE a.name = 'Milan Kundera';
 -- Find all books written by an author from China or the UK.
+SELECT * FROM books b
+JOIN authors a ON b.author_id = a.id
+WHERE a.nationality IN ('China', 'United Kingdom');
 
 -- Find out how many books Albert Camus wrote.
-
+SELECT COUNT(*) FROM books b
+JOIN authors a ON b.author_id = a.id
+WHERE a.name = 'Albert Camus';
 -- Find out how many books were written by US authors.
-
+SELECT COUNT(*) FROM books b
+JOIN authors a ON b.author_id = a.id
+WHERE a.nationality = 'United States of America';
 -- Find all books written after 1930 by authors from Argentina.
-
+SELECT * FROM books b
+JOIN authors a ON b.author_id = a.id
+WHERE a.nationality = 'Argentina' AND b.publication_date > 1930;
 -- Find all books written before 1980 by authors not from the US.
-
+SELECT * FROM books b
+JOIN authors a ON b.author_id = a.id
+WHERE a.nationality != 'United States of America' AND b.publication_date < 1980;
 -- Find all authors whose names start with 'J'.
+SELECT * FROM authors
+WHERE name LIKE 'J%';
 
 -- Find all books whose titles contain 'the'.
-
+SELECT * FROM books
+WHERE title ILIKE '%the%';
 -- Find all authors who have written books with that start with the letter 'N'.
+SELECT a.* FROM authors a
+JOIN books b ON a.id = b.author_id
+WHERE b.title LIKE 'N%';
--- work/week-05/day-05/02. SQL-Library-Lab (D)/basic_queries.sql
+++ work/week-05/day-05/02. SQL-Library-Lab (D)/basic_queries.sql
@@ -4,30 +4,45 @@
 \x on     
 
 -- Get all information about all authors
-
+SELECT * FROM authors;
 -- Get just the name and birth year of all authors
-
+SELECT name, birth_year FROM authors;
 -- Get all authors born in the 20th centruy or later
-
+SELECT * FROM authors 
+WHERE birth_year >= 1900;
 -- Get all authors born in the USA
-
+SELECT * FROM authors
+WHERE nationality = 'United States of America';
 -- Get all books published on 1985
-
+SELECT * FROM books
+WHERE publication_date = 1985;
 -- Get all books published before 1989
-
+SELECT * FROM books
+WHERE publication_date < 1989;
 -- Get just the title of all books.
-
+SELECT title FROM books;
 -- Get just the year that 'A Dance with Dragons' was published
   -- Cry when you realize how long it's been
+  SELECT publication_date FROM books
+  WHERE title = 'A Dance with Dragons';
 
 -- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
-
+SELECT * FROM books
+WHERE title ILIKE '%the%';
 -- Add yourself as an author
-
+INSERT INTO authors(name, nationality, birth_year) VALUES ('Khalid Alduwaysan', 'Saudi', 2002);
+SELECT * FROM authors
+WHERE name = 'Khalid Alduwaysan';
 -- Add two books that you'd like to write (you can hard-code your id as the author id)
-
--- Update one of your books to have a new title
-
+INSERT INTO books(title, publication_date, author_id) VALUES ('Software Engineering Fundumentals', 2024, 9);
+-- -- Update one of your books to have a new title
+UPDATE books
+SET title = 'Artificial Intelligence Basics'
+WHERE title = 'Software Engineering Fundumentals';
 -- Delete your books
+DELETE FROM books
+WHERE title = 'Artificial Intelligence Basics';
 
 -- Delete your author entry
+DELETE FROM authors
+WHERE name = 'Khalid Alduwaysan';
