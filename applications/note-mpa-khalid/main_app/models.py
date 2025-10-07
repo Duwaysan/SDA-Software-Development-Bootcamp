a76a0f02054cbbc270896827863402b54246b6f2
Duwaysan <kduwaysan@gmail.com>
2025-10-08 00:36:07 +0300

    create Checklist class

--- applications/note-mpa-khalid/main_app/models.py
+++ applications/note-mpa-khalid/main_app/models.py
@@ -14,3 +14,16 @@ class Note(models.Model):
         def get_absolute_url(self):
             # Use the 'reverse' function to dynamically find the URL for viewing this cat's details
             return reverse('note-detail', kwargs={'note_id': self.id})
+        
+
+class Checklist(models.Model):
+    text = models.CharField(max_length=255)
+    is_done = models.BooleanField(default=False)
+
+    note = models.ForeignKey(Note, on_delete=models.CASCADE)
+
+    def __str__(self):
+        if self.is_done:
+            return f"{self.text} is done"
+        else:
+            return f"{self.text} is not done"
