77a47902dfee160a55d016c8f207dd6718444519
adevlinb <adevlin.booth@gmail.com>
2025-10-09 12:39:01 +0300

    completes catcolector auth and project

--- applications/cat-collector-mpa-devlin/main_app/urls.py
+++ applications/cat-collector-mpa-devlin/main_app/urls.py
@@ -3,7 +3,7 @@ from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
     # Routes will be added here
-    path('', views.home, name='home'),
+    path('', views.Home.as_view(), name='home'),
     path('about/', views.about, name='about'),
     path('cats/', views.cat_index, name='cat-index'),
     path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail'),
@@ -19,4 +19,5 @@ urlpatterns = [
     path('cats/<int:cat_id>/associate-toy/<int:toy_id>/', views.associate_toy, name='associate-toy'),
     path('cats/<int:cat_id>/remove-toy/<int:toy_id>/', views.remove_toy, name='remove-toy'),
     path('cats/<int:cat_id>/add-photo/', views.add_photo, name='add-photo'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- applications/cat-collector-mpa-devlin/main_app/views.py
+++ applications/cat-collector-mpa-devlin/main_app/views.py
@@ -4,17 +4,52 @@ from django.views.generic import ListView, DetailView # add these
 from django.urls import reverse
 from .models import Cat, Toy, Photo
 from .forms import FeedingForm, PhotoForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+# Import the login_required decorator
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-def home(request):
-    return render(request, 'home.html')
+class Home(LoginView):
+    template_name = 'home.html'
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in
+            login(request, user)
+            return redirect('cat-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
+    # Same as: 
+    # return render(
+    #     request, 
+    #     'signup.html',
+    #     {'form': form, 'error_message': error_message}
+    # )
 
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def cat_index(request):
-    cats = Cat.objects.all()
+    # old version - get ALL cats
+    # cats = Cat.objects.all()
+    cats = Cat.objects.filter(user=request.user)
     return render(request, 'cats/index.html', {'cats': cats })
 
+@login_required
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     # toys = Toy.objects.all()  # Fetch all toys <= old code!!! 
@@ -26,24 +61,35 @@ def cat_detail(request, cat_id):
         'cat': cat, 'feeding': feeding_form, 'toys': toys_cat_doesnt_have, "photo_form": photo_form 
     })
 
-class CatCreate(CreateView):
+
+class CatCreate(LoginRequiredMixin, CreateView):
     model = Cat
     fields = ['name', 'breed', 'description', 'age']
     # success_url = '/cats/'
 
     def get_success_url(self):
-        return reverse('cat_detail', kwargs={'cat_id': self.object.id})
+        return reverse('cat-detail', kwargs={'cat_id': self.object.id})
+    
+    # This inherited method is called when a
+    # valid cat form is being submitted
+    def form_valid(self, form):
+        # Assign the logged in user (self.request.user)
+        form.instance.user = self.request.user  # form.instance is the cat
+        # Let the CreateView do its job as usual
+        return super().form_valid(form)
 
 
-class CatUpdate(UpdateView):
+class CatUpdate(LoginRequiredMixin, UpdateView):
     model = Cat
     # Let's disallow the renaming of a cat by excluding the name field!
     fields = ['breed', 'description', 'age']
 
-class CatDelete(DeleteView):
+
+class CatDelete(LoginRequiredMixin, DeleteView):
     model = Cat
     success_url = '/cats/'
 
+@login_required
 def add_feeding(request, cat_id):
     # create a ModelForm instance using the data in request.POST
     form = FeedingForm(request.POST)
@@ -56,46 +102,37 @@ def add_feeding(request, cat_id):
         new_feeding.save()
     return redirect('cat-detail', cat_id=cat_id)
 
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
     model = Toy
     fields = '__all__'
 
-class ToyList(ListView):
+class ToyList(LoginRequiredMixin, ListView):
     model = Toy
 
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
     model = Toy
 
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
     model = Toy
     fields = ['name', 'color']
 
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
     model = Toy
     success_url = '/toys/'
 
+@login_required
 def associate_toy(request, cat_id, toy_id):
     # Note that you can pass a toy's id instead of the whole object
     Cat.objects.get(id=cat_id).toys.add(toy_id)
     return redirect('cat-detail', cat_id=cat_id)
 
+@login_required
 def remove_toy(request, cat_id, toy_id):
     # Note that you can pass a toy's id instead of the whole object
     Cat.objects.get(id=cat_id).toys.remove(toy_id)
     return redirect('cat-detail', cat_id=cat_id)
 
-def add_photo(request, cat_id):
-    # create a ModelForm instance using the data in request.POST
-    form = PhotoForm(request.POST)
-    # validate the form
-    if form.is_valid():
-        # don't save the form to the db until it
-        # has the cat_id assigned
-        new_photo = form.save(commit=False)
-        new_photo.cat_id = cat_id
-        new_photo.save()
-    return redirect('cat-detail', cat_id=cat_id)
-
+@login_required
 def add_photo(request, cat_id):
     form = PhotoForm(request.POST)
     if form.is_valid():
