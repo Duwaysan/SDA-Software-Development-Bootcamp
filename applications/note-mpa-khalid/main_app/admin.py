8ce1c710724fde157bf6e8f194c853cf64fdb1f6
Duwaysan <kduwaysan@gmail.com>
2025-10-09 16:40:32 +0300

    fixing time-zone

--- applications/note-mpa-khalid/main_app/admin.py
+++ applications/note-mpa-khalid/main_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Note, Checklist, Reaction, ActivityLog
-
+from django.contrib.auth.models import User
 admin.site.register(Note)
 admin.site.register(Checklist)
 admin.site.register(Reaction)
