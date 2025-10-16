f520f4559791c8a0beeb706ca52e5eda94f5ab51
adevlinb <adevlin.booth@gmail.com>
2025-10-16 15:05:41 +0300

    adds day one cat collector spa

---
+++ applications/cat-collector-spa-devlin/frontend/src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function AboutPage() {
+    return (<>
+        <h1>About the Cat Collector</h1>
+        <hr />
+        <p className="page-content">
+          I like long romantic walks down the produce aisle. Also, I am currently job
+          seeking!
+        </p>
+    </>)
+}
