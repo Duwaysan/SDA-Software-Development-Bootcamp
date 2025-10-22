aaf41203b493181d4fbf7bf416cc235863adad51
Duwaysan <kduwaysan@gmail.com>
2025-10-22 10:28:38 +0300

    create initial models & migrate

--- applications/note-spa-khalid/backend/main_app/models.py
+++ applications/note-spa-khalid/backend/main_app/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from datetime import date
 
 class Note(models.Model):
     title = models.CharField(max_length=100)
@@ -8,3 +7,17 @@ class Note(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    person_name = models.CharField(max_length=50)
+    comment = models.TextField()
+
+class Photo(models.Model):
+    title = models.CharField(max_length=50)
+    url = models.TextField()
+
+class Event(models.Model):
+    
+    name = models.CharField(max_length=50)
+    description= models.CharField(max_length=100)
+    event_time =  models.DateTimeField()
