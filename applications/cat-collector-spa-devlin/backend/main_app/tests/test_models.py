ca5a6edbc396952e61958a91bb11d668355147b9
adevlinb <adevlin.booth@gmail.com>
2025-10-27 09:02:47 +0300

    push second testing items

--- applications/cat-collector-spa-devlin/backend/main_app/tests/test_models.py
+++ applications/cat-collector-spa-devlin/backend/main_app/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Cat, Feeding, Photo, Toy
+from ..models import Cat, Feeding, Photo, Toy
 from datetime import date
 # Create your tests here.
 
